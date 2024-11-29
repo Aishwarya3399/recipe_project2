cf29f2c17cec2c8ee484f1c6d382bf179866cdd8
Aishwarya Bharat Hulle <aishwaryahulle@gmail.com>
2024-11-29 10:47:13 +0530

    Updated package files and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ import { WishlistProvider } from './context/WishlistContext';
 import { AuthProvider } from './context/AuthContext';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-const CuisineDetailPage = lazy(() => import('./components/CuisineDetailPage'));
 
 // Lazy-loaded components for better performance
+const CuisineDetailPage = lazy(() => import('./components/CuisineDetailPage'));
 const Banner = lazy(() => import('./components/Banner'));
 const AboutUs = lazy(() => import('./components/AboutUs'));
 const KitchenTips = lazy(() => import('./components/KitchenTips'));
@@ -81,83 +81,37 @@ const App = () => {
     return (
         <AuthProvider>
             <WishlistProvider>
-                <Router>
+                {/* Add `basename` for deployment */}
+                <Router basename="/recipe_project2">
                     <ScrollToTop />
                     <div className="App">
-                    <Suspense fallback={<div style={{ minHeight: '300px' }}>Loading...</div>}>
+                        <Suspense fallback={<div style={{ minHeight: '300px' }}>Loading...</div>}>
                             <Routes>
-                                {/* Public Route for Login/Signup */}
                                 <Route path="/login" element={<LoginSignup />} />
-
-                                {/* Protected Route for Adding Recipe */}
                                 <Route path="/add-recipe" element={<ProtectedRoute element={<AddRecipePage />} />} />
-
-                                {/* Home Page */}
                                 <Route path="/" element={<Layout />} />
-
-                                {/* Recipe Detail Page */}
                                 <Route path="/recipe/:id" element={<Layout><Recipe /></Layout>} />
-
-                                   {/* Recipe Detail Page */}
-                                   <Route path="/recipe1/:id" element={<Layout><Recipe1 /></Layout>} />
-
-                                {/* Search Recipe Page */}
+                                <Route path="/recipe1/:id" element={<Layout><Recipe1 /></Layout>} />
                                 <Route path="/search" element={<SearchRecipe />} />
-
-                                {/* Thali Detail Pages */}
                                 <Route path="/thali" element={<Layout><ThaliDetailPage /></Layout>} />
                                 <Route path="/thali/:thali" element={<Layout><ThaliDetailPage /></Layout>} />
-
-                                {/* All Recipes Page */}
                                 <Route path="/all-recipes" element={<Layout><AllRecipesPage /></Layout>} />
-
-                                {/* Wishlist Page */}
                                 <Route path="/wishlist" element={<Layout><WishlistPage /></Layout>} />
-
-                                {/* About Us Page */}
                                 <Route path="/about-us" element={<Layout><AboutUs /></Layout>} />
-
-                                {/* Kitchen Tips Page */}
                                 <Route path="/kitchen-tips" element={<Layout><KitchenTips /></Layout>} />
-
-                                {/* Dynamic Recipe Category Page */}
                                 <Route path="/recipes/:category" element={<Layout><RecipePage /></Layout>} />
-
-                                {/* Cuisine Detail Page */}
                                 <Route path="/cuisine/:cuisine" element={<Layout><CuisineDetailPage /></Layout>} />
-
-                                {/* Occasion Detail Page */}
                                 <Route path="/occasion/:occasionName" element={<Layout><OccasionDetailPage /></Layout>} />
-
-                                {/* Meal Detail Page */}
                                 <Route path="/meal/:mealType" element={<Layout><MealDetailPage /></Layout>} />
-
-                                {/* Dietary Category Pages */}
                                 <Route path="/diet/:dietType/:subDietType?" element={<Layout><DietDetailPage /></Layout>} />
-
-                                {/* Taste Detail Page */}
                                 <Route path="/taste/:tasteName" element={<Layout><TasteDetailPage /></Layout>} />
-
-                                {/* Ingredients Detail Page */}
                                 <Route path="/ingredients/:ingredientName" element={<Layout><IngredientsDetailPage /></Layout>} />
-
-                                 {/* Ingredients Detail Page */}
                                 <Route path="/ingredients/:All Ingredients" element={<Layout><AllIngredientsPage /></Layout>} />
-
-                                {/* Thela Menu Page */}
                                 <Route path="/thela" element={<Layout><ThelaMenu /></Layout>} />
-
-                                {/* Cake and Ice Cream Pages */}
                                 <Route path="/cake" element={<Layout><Cake /></Layout>} />
                                 <Route path="/ice-cream" element={<Layout><Icecream /></Layout>} />
-
-                                {/* Easy Recipes Page */}
                                 <Route path="/easy-recipes" element={<Layout><EasyRecipesPage /></Layout>} />
-
-                                {/* FAQs Page */}
                                 <Route path="/faqs" element={<Layout><Faq /></Layout>} />
-
-                                {/* Blog, Privacy Policy, Contact, Terms Pages */}
                                 <Route path="/privacy-policy" element={<Layout><Privacy /></Layout>} />
                                 <Route path="/contact" element={<Layout><Contact /></Layout>} />
                                 <Route path="/blog" element={<Layout><Blog /></Layout>} />
