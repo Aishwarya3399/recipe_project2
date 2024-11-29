7e2d868bed792e6025fb9f4765c8b0392a4f0c80
Aishwarya Bharat Hulle <aishwaryahulle@gmail.com>
2024-11-29 17:05:39 +0530

    new file uploading

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Export Vite configuration
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/recipe_project2/', // Ensure base path is correct for GitHub Pages
+  build: {
+    outDir: 'dist', // Ensure the build goes to the 'dist' folder
+    rollupOptions: {
+      input: 'index.html', // Ensure Vite knows where to find the main HTML entry file
+    },
+  },
+});
+
