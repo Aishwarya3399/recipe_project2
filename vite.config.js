52be204248eeda11f7d14d74aee1e468d361ee41
Aishwarya Bharat Hulle <aishwaryahulle@gmail.com>
2024-11-30 12:38:07 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Export Vite configuration
 export default defineConfig({
   plugins: [react()],
-  base: '/recipe_project2/', // Ensure base path is correct for GitHub Pages
-  build: {
-    outDir: 'dist', // Ensure the build goes to the 'dist' folder
-    rollupOptions: {
-      input: 'index.html', // Ensure Vite knows where to find the main HTML entry file
-    },
-  },
+  base: '/recipe_project2/', // Ensure this matches your GitHub repository name
 });
+
 
